89b056254d795ebf34b7699b9f3b75f4a8b3684d
Palak Vora <palak.vora19@gmail.com>
2020-07-03 02:59:26 +0530

    Hello and Goodbye with manual server

--- Working/handlers/first.go
+++ Working/handlers/first.go
@@ -7,14 +7,14 @@ import (
 	"net/http"
 )
 
-type handle1 struct {
+type Handle1 struct {
 	l *log.Logger
 }
 
-func disp(l *log.Logger) *handle1 {
-	return &handle1(l)
+func Disp(l *log.Logger) *Handle1 {
+	return &Handle1{l}
 }
-func (h *handle1) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+func (h *Handle1) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("Hello World")
 	d, err := ioutil.ReadAll(r.Body) //ioutil returns data and err if any ; read Nic from request body in command line using ioutil
 	if err != nil {
@@ -24,6 +24,6 @@ func (h *handle1) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Bold")
 		return //return needed cause http.error doesnt terminate go program
 	}
-	log.Printf("Data %s \n", d) //while executing curl make sure to use -d 'Data' ; Invoke webrequest can be removed by Remove-item alias:curl command, ensures that curl is running
-	fmt.Fprintf(rw, "Hello %s \n", d)
+	//log.Printf("Data %s \n", d) //while executing curl make sure to use -d 'Data' ; Invoke webrequest can be removed by Remove-item alias:curl command, ensures that curl is running
+	fmt.Fprintf(rw, "Hello %s", d)
 }
--- Working/main.go
+++ Working/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"GitVsGolang/Working/handlers"
+	"log"
 	"net/http"
+	"os"
+	"time"
+	"context"
+	"os/signal"
+	
 )
 
 func main() {
@@ -15,7 +22,33 @@ func main() {
 	//	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
 	//		log.Println("Goodbye World")
 	//	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.Disp(l)
+	gh :=handlers.NewGoodbye(l)	
+	sm:=http.NewServeMux()
+	sm.Handle("/",hh)
+	sm.Handle("/goodbye",gh)
+	s:=&http.Server{
+		Addr:":9090",
+		Handler:sm,
+		IdleTimeout: 10*time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}   //Created a manual server
+	//http.ListenAndServe(":9090", sm) //Constructs http server and registers default handler
+	go func(){
+		err:=s.ListenAndServe()
+		if err!=nil{
+			l.Fatal(err)
+		}
+	}()
+	sigChan:= make(chan os.Signal)
+	signal.Notify(sigChan,os.Interrupt)
+	signal.Notify(sigChan,os.Kill)
 
-	http.ListenAndServe(":9090", nil) //Constructs http server and registers default handler
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown",sig)
 
+	tc,_:=context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc) //waits for the exit of all the existing clients and completion of requests without accepting any more request
 }
